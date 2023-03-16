d777476e80b89a8a497b16b34d0ee997c082ee7c
steveruu <39307626+steveruu@users.noreply.github.com>
2023-03-16 11:27:40 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,20 @@
-let strip = neopixel.create(DigitalPin.P16, 48, NeoPixelMode.RGB);
-strip.showRainbow(0,359);
-strip.show();
+function ping(
+    trig: DigitalPin,
+    echo: DigitalPin,
+    maxcm_distance = 500): number {
+
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2); // reset
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10); // pipa 10 ms
+        pins.digitalWritePin(trig, 0);
+
+        let duration = pins.pulseIn(echo, PulseValue.High, 1/340 * 10);
+    return duration; // v mikrosekundach
+}
 
 basic.forever(function() {
-    strip.rotate();
-    strip.show();
-    basic.pause(10);
+    console.logValue("duration", ping(DigitalPin.P8, DigitalPin.P15));
+    basic.pause(3000)
 })
-
-
-  
